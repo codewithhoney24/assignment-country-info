419dacbfb113c0f1a41455938b14b4b819783ddc
NOUSHEEN ATIF <161060487+codewithhoney24@users.noreply.github.com>
2024-09-30 05:22:09 -0700

    data.ts

--- app/country/data.ts
+++ app/country/data.ts
@@ -5,9 +5,9 @@ export interface Country {
 }
 
 export const countries: Record<string, Country> = {
-  canada: { name: "Pakistan", population: "241  million", capital: " Islamabad" },
-  usa: { name:     " United States", population: "331 million", capital: "Washington, D.C." },
-  germany: { name: " Germany", population: "83 million", capital: "Berlin" },
+  canada: { name: "Pakistan", population: "241  million", capital : " Islamabad" },
+  usa: { name: " United States", population: "331  million", capital: "Washington, D.C." },
+  germany: { name: " Germany",  population : "83 million", capital: "Berlin" },
   australia: { name: "Australia", population: "25 million", capital: "Canberra" },
-  japan: { name:    " Japan", population: "125 million", capital: "Tokyo" }
+  japan: { name: " Japan", population: "125  million", capital: "Tokyo" }
 };
